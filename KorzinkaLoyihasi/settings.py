59b185f64357e4154185667e48d5b2e3c94d0de9
MirshohidM <mirshohidmirvohidov@gmail.com>
2021-09-11 23:59:59 +0500

    added silk

--- KorzinkaLoyihasi/settings.py
+++ KorzinkaLoyihasi/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -43,7 +44,7 @@ INSTALLED_APPS = [
     #'rest_auth',
     #'djoser',
     #'rest_framework_simplejwt',
-
+    'silk',
 
     'korzinka_api'
 ]
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'KorzinkaLoyihasi.urls'
@@ -151,8 +153,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
